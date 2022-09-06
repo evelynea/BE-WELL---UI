5e2f5b177192c65b8d24fdf2d7aad1dc931ff377
NSHUTI <jacknshuti11@gmail.com>
2022-09-06 09:19:52 +0200

    Added Components

--- src/ui/landing/landing.jsx
+++ src/ui/landing/landing.jsx
@@ -1,86 +1,27 @@
 import "./landing.css";
+import "../components/navbar/navbar";
+import "../components/heading/heading";
 import communicable from "../../images/communicable  diseae 1.png";
 import check from "../../images/checkup.png";
 import pills from "../../images/pills.png";
-import footer from "../../images/footer.png";
-import { IoIosArrowForward } from "react-icons/io";
-import { FaFacebookF } from "react-icons/fa";
-import { IoLogoInstagram } from "react-icons/io";
-import { BsTwitter } from "react-icons/bs";
-import { MdEmail } from "react-icons/md";
-import { FiSearch } from "react-icons/fi";
+// import footer from "../../images/footer.png";
 import { IconButton } from "@material-ui/core";
 import {RiArrowLeftSLine} from "react-icons/ri";
 import {RiArrowRightSLine} from "react-icons/ri"
-import "../footer/footer";
-import Footer from "../footer/footer";
+import "../components/footer/footer";
+import Footer from "../components/footer/footer";
+import Navbar from "../components/navbar/navbar";
+import Heading from "../components/heading/heading";
 
 function Landing() {
   return (
     <div className="container">
       <div className="navbar">
-        <a href="#">Do You Have An Idea For Be Well To Cover? Submit It Here!</a>
+        <Navbar />
       </div>
-      <div className="heading">
-        <div className="header">
-          <h2>Language </h2>
-          <IconButton>
-            <IoIosArrowForward className="iconheader" />
-          </IconButton>
-        </div>
-        <div className="image-container"></div>
-        <div className="img-block">
-          <h2>Most Popular</h2>
-          <ul>
-            <li>IMPLEMENT THE WHO FRAMEWORK CONVECTION ON TOBACCO CONTROL.</li>
-            <li>
-              UNIVERSAL ACCESS TO SEXUAL AND REPRODUCTIVE CARE, FAMILY PLANNING
-              AND EDUCATION.
-            </li>
-            <li>
-              REDUCE MORTALITY FROM NON-COMMUNICABLE DISEASES AND PROMOTE MENTAL
-              HEALTH.
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="title">
-        <h1>Be Well</h1>
-        <p>Tuesday, August 30, 2022</p>
-      </div>
-      <div className="icons-container">
-        <div className="icons">
-          <IconButton>
-            <FaFacebookF color="6C6AC7" />{" "}
-          </IconButton>
-          <IconButton>
-            <IoLogoInstagram color="C66565" />{" "}
-          </IconButton>
-          <IconButton>
-            <BsTwitter color="818DFF" />{" "}
-          </IconButton>
-          <IconButton>
-            <MdEmail color="797235" />{" "}
-          </IconButton>
-        </div>
-        <div className="active-tabs">
-          <p>
-            <a href="/">Diseases</a>{" "}
-          </p>
-          <p>
-            <a href="/">Drug</a>{" "}
-          </p>
-          <p>
-            <a href="/">Family</a>{" "}
-          </p>
-        </div>
-        <div className="search-icon">
-          <IconButton>
-            <FiSearch />
-          </IconButton>
-          <input type="text" placeholder="Type what you’re looking for" />
-        </div>
-      </div>
+     <div>
+       <Heading />
+     </div>
       <div id="P-diseases">
         <p>Diseases</p>
       </div><hr/>
