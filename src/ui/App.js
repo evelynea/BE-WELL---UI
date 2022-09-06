ea14fa511fc988e11b3264304ef2e959f0d586c7
evelynea <evelyneujeneza.com>
2022-09-06 11:09:17 +0200

    ok

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import '../ui/landing/landing'
 import Landing from "../ui/landing/landing";
+import News from "./news/news";
 function App() {
   return (
     <div className="App">
       <Landing />
+      {/* <News/> */}
     </div>
   );
 }
