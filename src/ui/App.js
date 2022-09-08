b88a4852ad4ce5277906c47719cb821ef2b25a71
evelynea <evelyneujeneza.com>
2022-09-08 09:03:43 +0200

    news

--- src/ui/App.js
+++ src/ui/App.js
@@ -5,8 +5,8 @@ import News from "./news/news";
 function App() {
   return (
     <div className="App">
-      <Landing />
-      {/* <News/> */}
+      {/* <Landing /> */}
+      <News/>
     </div>
   );
 }
